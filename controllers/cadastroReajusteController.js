3b63567a7162ea2ec2279812c49bdd0983bfcb27
Leandro Renato <leandrosantos.contabilidade@gmail.com>
2025-03-26 06:32:26 -0300

    Corrige nome da tabela para cadastro_reajuste

--- controllers/cadastroReajusteController.js
+++ controllers/cadastroReajusteController.js
@@ -1,7 +1,8 @@
+
 const db = require('../config/database');
 
 exports.getReajustes = (req, res) => {
-    db.query('SELECT * FROM Cadastro_Reajuste', (err, results) => {
+    db.query('SELECT * FROM cadastro_reajuste', (err, results) => {
         if (err) res.status(500).json({ error: err });
         else res.json(results);
     });
@@ -9,7 +10,7 @@ exports.getReajustes = (req, res) => {
 
 exports.createReajuste = (req, res) => {
     const { Data_reajuste, Grupo } = req.body;
-    db.query('INSERT INTO Cadastro_Reajuste (Data_reajuste, Grupo) VALUES (?, ?)', 
+    db.query('INSERT INTO cadastro_reajuste (Data_reajuste, Grupo) VALUES (?, ?)', 
         [Data_reajuste, Grupo], (err, result) => {
         if (err) res.status(500).json({ error: err });
         else res.json({ message: 'Reajuste cadastrado com sucesso!' });
@@ -19,7 +20,7 @@ exports.createReajuste = (req, res) => {
 exports.updateReajuste = (req, res) => {
     const { id } = req.params;
     const { Data_reajuste, Grupo } = req.body;
-    db.query('UPDATE Cadastro_Reajuste SET Data_reajuste = ?, Grupo = ? WHERE id = ?', 
+    db.query('UPDATE cadastro_reajuste SET Data_reajuste = ?, Grupo = ? WHERE id = ?', 
         [Data_reajuste, Grupo, id], (err, result) => {
         if (err) res.status(500).json({ error: err });
         else res.json({ message: 'Reajuste atualizado com sucesso!' });
@@ -28,7 +29,7 @@ exports.updateReajuste = (req, res) => {
 
 exports.deleteReajuste = (req, res) => {
     const { id } = req.params;
-    db.query('DELETE FROM Cadastro_Reajuste WHERE id = ?', [id], (err, result) => {
+    db.query('DELETE FROM cadastro_reajuste WHERE id = ?', [id], (err, result) => {
         if (err) res.status(500).json({ error: err });
         else res.json({ message: 'Reajuste deletado com sucesso!' });
     });
