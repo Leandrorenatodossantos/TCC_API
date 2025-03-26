437ab983cf0c911dfd67dfeaa14a07bbcfeb5616
Leandro Renato <leandrosantos.contabilidade@gmail.com>
2025-03-26 07:07:06 -0300

    Corrigido nome da tabela para cadastro_grupo

--- controllers/cadastroGrupoController.js
+++ controllers/cadastroGrupoController.js
@@ -1,7 +1,7 @@
 const db = require('../config/database');
 
 exports.getGrupos = (req, res) => {
-    db.query('SELECT * FROM Cadastro_Grupo', (err, results) => {
+    db.query('SELECT * FROM cadastro_grupo', (err, results) => {
         if (err) res.status(500).json({ error: err });
         else res.json(results);
     });
@@ -9,7 +9,7 @@ exports.getGrupos = (req, res) => {
 
 exports.createGrupo = (req, res) => {
     const { Grupo, Descricao } = req.body;
-    db.query('INSERT INTO Cadastro_Grupo (Grupo, Descricao) VALUES (?, ?)', 
+    db.query('INSERT INTO cadastro_grupo (Grupo, Descricao) VALUES (?, ?)', 
         [Grupo, Descricao], (err, result) => {
         if (err) res.status(500).json({ error: err });
         else res.json({ message: 'Grupo cadastrado com sucesso!' });
@@ -19,7 +19,7 @@ exports.createGrupo = (req, res) => {
 exports.updateGrupo = (req, res) => {
     const { id } = req.params;
     const { Grupo, Descricao } = req.body;
-    db.query('UPDATE Cadastro_Grupo SET Grupo = ?, Descricao = ? WHERE id = ?', 
+    db.query('UPDATE cadastro_grupo SET Grupo = ?, Descricao = ? WHERE id = ?', 
         [Grupo, Descricao, id], (err, result) => {
         if (err) res.status(500).json({ error: err });
         else res.json({ message: 'Grupo atualizado com sucesso!' });
@@ -28,7 +28,7 @@ exports.updateGrupo = (req, res) => {
 
 exports.deleteGrupo = (req, res) => {
     const { id } = req.params;
-    db.query('DELETE FROM Cadastro_Grupo WHERE id = ?', [id], (err, result) => {
+    db.query('DELETE FROM cadastro_grupo WHERE id = ?', [id], (err, result) => {
         if (err) res.status(500).json({ error: err });
         else res.json({ message: 'Grupo deletado com sucesso!' });
     });
